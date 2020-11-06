96ec7340457f02ef42d1ee375c5323c935a12dfc
Cristian Nazareno Gonzalez <43855102+crisgonzalez94@users.noreply.github.com>
2020-11-06 19:17:06 -0300

    1.0.1 is ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -22,6 +23,11 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      // set status bar color
+      this.statusBar.backgroundColorByHexString('#ffd500');
+
+
     });
   }
 }
