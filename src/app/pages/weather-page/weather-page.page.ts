80c831208288ac25fb9dca1fe8c84d4e2c135d17
Cristian Nazareno Gonzalez <43855102+crisgonzalez94@users.noreply.github.com>
2020-11-07 01:16:00 -0300

    added function that shows weather of the week

--- src/app/pages/weather-page/weather-page.page.ts
+++ src/app/pages/weather-page/weather-page.page.ts
@@ -6,6 +6,7 @@ import { ModalController } from '@ionic/angular';
 import { WeatherService } from 'src/app/services/weather.service';
 //Import the interfaces
 import { WeatherDate } from 'src/app/interfaces/interfaces';
+import { WeatherWeekDate } from 'src/app/interfaces/interfaces';
 
 @Component({
   selector: 'app-weather-page',
@@ -17,18 +18,25 @@ export class WeatherPagePage implements OnInit {
   //Variables for show weather
   icono:string;
   weather:WeatherDate;
+  weekWeather:WeatherWeekDate;
   city:string;
   country: string;
   //Var for show the progressbar
   showLoader: boolean = true;
 
-  constructor(private modalController: ModalController ,  private weatherSevice : WeatherService ) { }
+  //Variables for get date
+  year: number;
+  month: number;
+  day: number;
+  dayOfWeek:number;
+  date = new Date;
+  week: string[] = [];
+
+  constructor(private modalController: ModalController ,  private weatherSevice : WeatherService) { }
 
   ngOnInit(){
-
-    console.log(this.country);
-
     this.getWeather();
+    this.getWeekWeather();
   }
 
   //Get weather from service and api
@@ -38,10 +46,38 @@ export class WeatherPagePage implements OnInit {
     this.weatherSevice.getWeather(this.city , this.country).subscribe( (resp:WeatherDate) => {
       this.weather = resp;
 
+      //Get date
+      this.year = parseInt(this.weather.data[0].datetime.split('-')[0]);
+      this.month = parseInt(this.weather.data[0].datetime.split('-')[1]);
+      this.day = parseInt(this.weather.data[0].datetime.split('-')[2]);
+      //Is there an error in this api , the month is one more
+      for (let i = 0; i < 7; i++) {
+        this.date = new Date(this.year , this.month -1 , this.day + i , 0 , 0);
+        this.dayOfWeek = this.date.getDay();
+        switch (this.dayOfWeek) {
+          case 0: this.week.push("Su");break;
+          case 1: this.week.push("Mo");break;
+          case 2: this.week.push("Tu");break;
+          case 3: this.week.push("We");break;
+          case 4: this.week.push("Th");break;
+          case 5: this.week.push("Fr");break;
+          case 6: this.week.push("Sa");break;
+        }
+      }
+
+      console.log(this.week);
+
       this.hideProgressBar();
     });
-
   }
+
+
+  getWeekWeather(){
+    this.weatherSevice.getWeekWeather(this.city , this.country).subscribe( (resp:WeatherWeekDate) => {
+      this.weekWeather = resp;
+    });
+  }
+
 
   closeModal(){
     /*Cerrar Modal sin Argumentos*/
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -13,4 +13,8 @@ export class WeatherService {
     return this.http.get(`https://api.weatherbit.io/v2.0/current?city=${cityName}&country=${countryCode}&key=22df51fa8b68413799305f2f2ab46821&units=F&lang=en`);
   }
 
+  getWeekWeather(cityName , countryCode){
+    return this.http.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&country=${countryCode}&key=22df51fa8b68413799305f2f2ab46821&lang=en`);
+  }
+
 }
